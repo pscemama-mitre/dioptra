d0fdd193d2b328374f16c03f2ba12f30bd2032c5
Keith Manville <kmanville@mitre.org>
2024-03-29 11:12:19 -0400

    feat: implement v1 schemas for models

--- src/dioptra/restapi/routes.py
+++ src/dioptra/restapi/routes.py
@@ -37,6 +37,8 @@ V1_ENTRYPOINTS_ROUTE = "entrypoints"
 V1_EXPERIMENTS_ROUTE = "experiments"
 V1_JOBS_ROUTE = "jobs"
 V1_MLFLOW_RUNS_ROUTE = "mlflow_runs"
+V1_MODELS_ROUTE = "models"
+V1_PLUGINS_ROUTE = "plugins"
 V1_PLUGIN_PARAMETER_TYPES_ROUTE = "pluginParameterTypes"
 V1_PLUGINS_ROUTE = "plugins"
 V1_QUEUES_ROUTE = "queues"
@@ -85,6 +87,7 @@ def register_v1_routes(api: Api) -> None:
     from .v1.experiments.controller import api as experiments_api
     from .v1.jobs.controller import api as jobs_api
     from .v1.mlflow_runs.controller import api as mlflow_runs_api
+    from .v1.models.controller import api as models_api
     from .v1.plugin_parameter_types.controller import api as plugin_parameter_types_api
     from .v1.plugins.controller import api as plugins_api
     from .v1.queues.controller import api as queues_api
@@ -95,6 +98,8 @@ def register_v1_routes(api: Api) -> None:
     api.add_namespace(experiments_api, path=f"/{V1_ROOT}/{V1_EXPERIMENTS_ROUTE}")
     api.add_namespace(jobs_api, path=f"/{V1_ROOT}/{V1_JOBS_ROUTE}")
     api.add_namespace(mlflow_runs_api, path=f"/{V1_ROOT}/{V1_MLFLOW_RUNS_ROUTE}")
+    api.add_namespace(models_api, path=f"/{V1_ROOT}/{V1_MODELS_ROUTE}")
+    api.add_namespace(plugins_api, path=f"/{V1_ROOT}/{V1_PLUGINS_ROUTE}")
     api.add_namespace(
         plugin_parameter_types_api, path=f"/{V1_ROOT}/{V1_PLUGIN_PARAMETER_TYPES_ROUTE}"
     )
